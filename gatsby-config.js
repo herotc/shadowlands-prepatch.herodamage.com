3f915cddd47e96216e4ffe26603755d553f400fa
Quentin Giraud <dev@aethys.io>
2020-10-13 08:59:04 +0200

    build: swap to www.herodamage.com

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Hero Damage',
-    url: 'https://shadowlands-prepatch.herodamage.com',
+    url: 'https://www.herodamage.com',
     github: 'https://github.com/herotc/shadowlands-prepatch.herodamage.com',
     description: 'Class theorycrafting community, latest simulation results and resources -based on SimulationCraft- for World of Warcraft.',
     keywords: 'herotc, herodamage, world of warcraft, shadowlands prepatch, wow sl prepatch, theorycrafting, simulationcraft, simc',
